5e5bbfa792d23b0e4bfbe6b1f691d491585faccd
Vlad Tihomirov <7292963@mail.ru>
2025-12-31 13:37:27 +0300

    add todo

--- src/main/java/com/zjloybojlk/coworkingSpaceBooking/Main.java
+++ src/main/java/com/zjloybojlk/coworkingSpaceBooking/Main.java
@@ -1,4 +1,11 @@
 package com.zjloybojlk.coworkingSpaceBooking;
 
+import com.sun.jdi.PathSearchingVirtualMachine;
+
 public class Main {
+    public static void main(String[] args) {
+        //TODO add postgresql with docker container
+        //Do methods in DBManager and CRUD in DESKDAOPImpl and prototype CRUD in interface
+        //and others things
+    }
 }
