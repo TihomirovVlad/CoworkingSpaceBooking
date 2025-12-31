097712e76d1b4e42bd4a4adb685581664a89ad4a
Vlad Tihomirov <7292963@mail.ru>
2025-12-31 13:39:45 +0300

    todo

--- src/main/java/com/zjloybojlk/coworkingSpaceBooking/dao/DeskDAO.java
+++ src/main/java/com/zjloybojlk/coworkingSpaceBooking/dao/DeskDAO.java
@@ -1,4 +1,5 @@
 package com.zjloybojlk.coworkingSpaceBooking.dao;
 
 public interface DeskDAO {
+    //add CRUD prototypes
 }
--- src/main/java/com/zjloybojlk/coworkingSpaceBooking/dao/DeskDAOImpl.java
+++ src/main/java/com/zjloybojlk/coworkingSpaceBooking/dao/DeskDAOImpl.java
@@ -2,5 +2,5 @@ package com.zjloybojlk.coworkingSpaceBooking.dao;
 
 public class DeskDAOImpl implements DeskDAO {
 
-
+    // add CRUD realization
 }
