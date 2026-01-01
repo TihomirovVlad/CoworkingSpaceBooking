c37fba9cd27e83075ca91808e9815f5c28a9dff2
Vlad Tihomirov <7292963@mail.ru>
2026-01-01 17:53:00 +0300

    add fields and constructors(without params, /w params)

--- src/main/java/com/zjloybojlk/coworkingSpaceBooking/entity/Desk.java
+++ src/main/java/com/zjloybojlk/coworkingSpaceBooking/entity/Desk.java
@@ -1,8 +1,31 @@
 package com.zjloybojlk.coworkingSpaceBooking.entity;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class Desk {
 
-    private int id;
-    private int number;
-    private boolean isOccupied;
+    private Integer id;
+    private String deskNumber; // example: "C123" "X012"
+    private String description;
+    private String type;
+    private BigDecimal pricePerHour;
+    private Boolean isOccupied;
+    private LocalDateTime createdAt;
+
+    public Desk() {
+        this.type = "STANDARD";
+        this.isOccupied = true;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public Desk(String deskNumber, String description, BigDecimal pricePerHour) {
+        this();
+        this.deskNumber = deskNumber;
+        this.description = description;
+        this.pricePerHour = pricePerHour;
+    }
+
+    //add String getOccupiedStatus,String getFormattedPrice, getters/setters, toString
+
 }
